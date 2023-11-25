f4a0695b8fe29f165d3278298e906169ca532d11
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2023-11-25 14:39:08 +0530

    Add tailwind and navigation

---
+++ src/navigation/index.js
@@ -0,0 +1,18 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../pages/HomeScreen";
+import AboutScreen from "../pages/AboutScreen";
+const Stack = createNativeStackNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/pages/HomeScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="">Home Screen</Text>
+    </SafeAreaView>
+  );
+}
